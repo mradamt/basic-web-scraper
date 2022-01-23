6d70bcd2d962015ad2311d4e50b1bff961a51102
Adam Tyler <mr.adamtyler@gmail.com>
2022-01-22 16:19:55 -0800

    discover anti web scraper tech, pause project

--- webscraper.py
+++ webscraper.py
@@ -1,3 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
+url = ''
+response = requests.get(url)
+content = BeautifulSoup(response.content, "html.parser").prettify()
+
+print (response.content)
